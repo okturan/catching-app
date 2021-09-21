7c1c2c96596641e0b64b8d600bfebf06b4be0083
Sedef Cakmak <cakmaksedef@gmail.com>
2021-09-21 12:22:02 +0300

    Completed the route and the skeleton of controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :events, only: [ :index, :show, :new, :create, :edit, :update ] do
-  resources :bookings, only: [ :new, :create ]
+  resources :events, only: [ :index, :show, :new, :create, :edit, :update ]
 end
