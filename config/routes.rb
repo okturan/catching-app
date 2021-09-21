f954e0f4ebdb9e762718e18e3fda314593f316e9
Sedef Cakmak <cakmaksedef@gmail.com>
2021-09-21 12:19:07 +0300

    Event controller'

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :events, only: [ :index, :show, :new, :create, :edit, :update ] do
+  resources :bookings, only: [ :new, :create ]
 end
