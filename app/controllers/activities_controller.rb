332d754a996389b54d6de1d50793ba04756bb44c
Ege Cakmak <egecakmak@hotmail.com>
2021-09-21 15:37:12 +0300

    activity migrations, activity controller.rb created

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,29 @@
+class ActivitiesController < ApplicationController
+  def new
+    @activity = Activity.find(params[:activity_id])
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    @activity.event = current_event
+    @activity.save
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def destroy
+    @activity = Activity.find(params[:id])
+    @activity.destroy
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name, :activity_type, :start_time, :end_time)
+  end
+end
