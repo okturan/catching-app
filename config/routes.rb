56f80a41de2e79095e8b398c1e61c2eb8c65a7ed
Sedef Cakmak <cakmaksedef@gmail.com>
2021-09-22 14:10:22 +0300

    Added JS to Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :events, only: [ :index, :show, :new, :create, :edit, :update ]
+  resource :dashboard, only: :show
 end
