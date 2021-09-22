7341b32e399b315ced0732889a6cb3eae0c5b13c
Ege Cakmak <egecakmak@hotmail.com>
2021-09-22 17:56:10 +0300

    events controller, activity views, cards scss

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,17 @@
+class EventsController < ApplicationController
+  def create
+    @event = Event.new(list_params)
+    @event.user = current_user
+    @event.save
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :activities, only: [ :index, :show, :new, :create ] do
+    resources :events, only: [ :new, :create, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
