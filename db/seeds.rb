f683cb8b65d0c90fbdaa4a28aedbdcc3e163425b
Okan Erturan <okanert@gmail.com>
2021-09-23 15:47:25 +0300

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Event.create(name: 'meeting', user_id: '1')
-Event.create(name: 'meeting2', user_id: '1')
+Activity.destroy_all
+Event.destroy_all
+User.destroy_all
 
-netflix = { name: 'Netflix', duration: 12, description: 'so much fun', event_id: 1 }
-gaming = { name: 'Gaming', duration: 1, description: 'fun times', event_id: 1 }
+user1 = User.create(email: "okan@test.com", password: "123456")
+
+event1 = Event.create(name: 'meeting', user: user1)
+event2 = Event.create(name: 'meeting2', user: user1)
+
+
+netflix = { name: 'Netflix', duration: 12, description: 'so much fun', event: event1 }
+gaming = { name: 'Gaming', duration: 1, description: 'fun times', event: event1 }
 
 [netflix, gaming].each do |attributes|
   activity = Activity.create!(attributes)
